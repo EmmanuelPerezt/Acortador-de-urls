bbed81dd90147828c725b4b74d32a43e8469598b
EmmanuelPerezt <emmanuel.pereztj@gmail.com>
2025-01-07 19:23:24 -0600

    initial commit

---
+++ src/main/java/com/acortador/AcortadorDeUrlsApplication.java
@@ -0,0 +1,13 @@
+package com.acortador;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AcortadorDeUrlsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AcortadorDeUrlsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/acortador/AcortadorDeUrlsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.acortador;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AcortadorDeUrlsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
